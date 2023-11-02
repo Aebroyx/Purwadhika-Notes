bd5ab8b1282cc459d7bc8f044e2b1f1e9543bdf3
Aebroyx <sangalabror@gmail.com>
2023-11-02 14:13:57 +0700

    day 8

---
+++ Module 1 (Fundamental)/Testing.js
@@ -0,0 +1,9 @@
+class Market {
+    constructor(item, stock) {
+        this.item = item,
+        this.stock = stock
+    }
+}
+
+const market = [new Market('Apple', 10), new Market('Apple', 10), new Market('Apple', 10)]
+console.log(market)
