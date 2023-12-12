fe2d183aa4185606bca3e8311fd864ff0dfe66ca
Aebroyx <41261433+Aebroyx@users.noreply.github.com>
2023-12-12 13:18:46 +0700

    expense api beta complete

--- Module 3 (Backend)/expense-api/index.js
+++ Module 3 (Backend)/expense-api/index.js
@@ -8,17 +8,163 @@ const fs_1 = __importDefault(require("fs"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 const port = 5000;
+// Get Expense List
 app.get('/expenses', (req, res) => {
     try {
-        const data = JSON.parse(fs_1.default.readFileSync('./database/db.json', 'utf-8'));
-        console.log(data);
-        res.send({
+        const database = JSON.parse(fs_1.default.readFileSync('./database/db.json', 'utf-8'));
+        res.status(200).send({
             error: false,
             message: "Get Expenses List Success!",
-            data: {}
+            data: database
         });
     }
     catch (error) {
+        res.status(500).send({
+            error: true,
+            message: error
+        });
+    }
+});
+// Get Expense Details
+app.get('/expenses/:id', (req, res) => {
+    try {
+        const database = JSON.parse(fs_1.default.readFileSync('./database/db.json', 'utf-8'));
+        const { id } = req.params;
+        let details = null;
+        for (const item of database.expenses) {
+            if (item.id === Number(id)) {
+                details = item;
+            }
+        }
+        res.status(200).send({
+            error: false,
+            message: `Get expenses ${id} Success!`,
+            data: details
+        });
+    }
+    catch (error) {
+        res.status(500).send({
+            error: true,
+            message: error
+        });
+    }
+});
+// Create New Expense Data
+app.post('/expenses', (req, res) => {
+    try {
+        const database = JSON.parse(fs_1.default.readFileSync('./database/db.json', 'utf-8'));
+        const data = Object.assign({ id: database.expenses.length + 1 }, req.body);
+        database.expenses.push(data);
+        fs_1.default.writeFileSync('./database/db.json', JSON.stringify(database));
+        res.status(200).send({
+            error: false,
+            message: 'Create new expense Success!',
+            data: null
+        });
+    }
+    catch (error) {
+        res.status(500).send({
+            error: true,
+            message: error
+        });
+    }
+});
+// Edit Expense Data
+app.put('/expenses/:id', (req, res) => {
+    try {
+        const database = JSON.parse(fs_1.default.readFileSync('./database/db.json', 'utf-8'));
+        const data = req.body;
+        const { id } = req.params;
+        database.expenses.forEach((items, index) => {
+            if (items.id === Number(id)) {
+                database.expenses[index] = Object.assign(Object.assign({}, data), { id: Number(id) });
+            }
+        });
+        fs_1.default.writeFileSync('./database/db.json', JSON.stringify(database));
+        res.status(200).send({
+            error: false,
+            message: `Update expenses ${id} Success!`,
+            data: null
+        });
+    }
+    catch (error) {
+        res.status(500).send({
+            error: true,
+            message: error
+        });
+    }
+});
+// Delete Expense Data
+app.delete('/expenses/:id', (req, res) => {
+    try {
+        const database = JSON.parse(fs_1.default.readFileSync('./database/db.json', 'utf-8'));
+        const data = req.body;
+        const { id } = req.params;
+        database.expenses.map((item, index) => {
+            if (item.id === Number(id)) {
+                database.expenses.splice(index, 1);
+            }
+        });
+        fs_1.default.writeFileSync('./database/db.json', JSON.stringify(database));
+        res.status(200).send({
+            error: false,
+            message: `Delete expenses ${id} Success!`,
+            data: null
+        });
+    }
+    catch (error) {
+        res.status(500).send({
+            error: true,
+            message: error
+        });
+    }
+});
+// Get Total Expense by Date Range
+app.get('/expenses-by-date', (req, res) => {
+    try {
+        const database = JSON.parse(fs_1.default.readFileSync('./database/db.json', 'utf-8'));
+        const range = req.query;
+        let totalExpense = 0;
+        database.expenses.forEach((item) => {
+            if (item.date >= range.startdate && item.date <= range.enddate) {
+                totalExpense += item.nominal;
+            }
+        });
+        res.status(200).send({
+            error: false,
+            message: 'Get Total Expense by Date Range Success!',
+            data: totalExpense
+        });
+    }
+    catch (error) {
+        res.status(500).send({
+            error: true,
+            message: error.message
+        });
+    }
+});
+// Get Total Expense by Category
+app.get('/expenses-by-category', (req, res) => {
+    try {
+        const database = JSON.parse(fs_1.default.readFileSync('./database/db.json', 'utf-8'));
+        const { category } = req.query;
+        let totalExpense = 0;
+        database.expenses.forEach((item) => {
+            if (item.category === category) {
+                totalExpense += item.nominal;
+            }
+        });
+        res.status(200).send({
+            error: false,
+            message: 'Get Total Expense by Category Success!',
+            data: totalExpense
+        });
+    }
+    catch (error) {
+        res.status(500).send({
+            error: true,
+            message: error.message
+        });
     }
 });
 app.listen(port, () => {
--- Module 3 (Backend)/expense-api/index.ts
+++ Module 3 (Backend)/expense-api/index.ts
@@ -13,21 +13,190 @@ interface IExpense {
     date: string
 }
 
+// Get Expense List
 app.get('/expenses', (req: Request, res: Response) => {
     try {
-        const data: IExpense = JSON.parse(fs.readFileSync('./database/db.json', 'utf-8'));
-        console.log(data)
-        res.send({
+        const database: {expenses: Array<IExpense>} = JSON.parse(fs.readFileSync('./database/db.json', 'utf-8'));
+
+        res.status(200).send({
             error: false,
             message: "Get Expenses List Success!",
-            data: {
-                
-            }
+            data: database
         })
     } catch (error) {
-        
+        res.status(500).send({
+            error: true,
+            message: error
+        })
     }
 });
+
+// Get Expense Details
+app.get('/expenses/:id', (req: Request, res: Response) => {
+    try {
+        const database: {expenses: Array<IExpense>}  = JSON.parse(fs.readFileSync('./database/db.json', 'utf-8'));
+        const { id } = req.params;
+
+        let details: IExpense | null = null;
+
+        for (const item of database.expenses) {
+            if (item.id === Number(id)) {
+                details = item;
+            }
+        }
+
+        res.status(200).send({
+            error: false,
+            message: `Get expenses ${id} Success!`,
+            data: details
+        })
+    } catch (error) {
+        res.status(500).send({
+            error: true,
+            message: error
+        })
+    }
+});
+
+// Create New Expense Data
+app.post('/expenses', (req: Request, res: Response) => {
+    try {
+        const database: { expenses: Array<IExpense> } = JSON.parse(fs.readFileSync('./database/db.json', 'utf-8'));
+
+        const data: IExpense = { id: database.expenses.length + 1,...req.body}
+
+        database.expenses.push(data)
+
+        fs.writeFileSync('./database/db.json', JSON.stringify(database))
+
+        res.status(200).send({
+            error: false,
+            message: 'Create new expense Success!',
+            data: null
+        })
+    } catch (error) {
+        res.status(500).send({
+            error: true,
+            message: error
+        })
+    }
+});
+
+// Edit Expense Data
+app.put('/expenses/:id', (req: Request, res: Response) => {
+    try {
+        const database: { expenses: Array<IExpense> } = JSON.parse(fs.readFileSync('./database/db.json', 'utf-8'));
+
+        const data: IExpense = req.body
+        const { id } = req.params;
+
+        database.expenses.forEach((items, index) => {
+            if (items.id === Number(id)) {
+                database.expenses[index] = {...data, id: Number(id)}
+            }
+        })
+
+        fs.writeFileSync('./database/db.json', JSON.stringify(database))
+
+        res.status(200).send({
+            error: false,
+            message: `Update expenses ${id} Success!`,
+            data: null
+        })
+    } catch (error) {
+        res.status(500).send({
+            error: true,
+            message: error
+        })
+    }
+})
+
+// Delete Expense Data
+app.delete('/expenses/:id', (req: Request, res: Response) => {
+    try {
+        const database: { expenses: Array<IExpense> } = JSON.parse(fs.readFileSync('./database/db.json', 'utf-8'));
+
+        const data: IExpense = req.body
+        const { id } = req.params;
+
+        database.expenses.map((item, index) => {
+            if (item.id === Number(id)) {
+                database.expenses.splice(index, 1)
+            }
+        })
+
+        fs.writeFileSync('./database/db.json', JSON.stringify(database))
+
+        res.status(200).send({
+            error: false,
+            message: `Delete expenses ${id} Success!`,
+            data: null
+        })
+    }
+    catch (error) {
+        res.status(500).send({
+            error: true,
+            message: error
+        })
+    }
+})
+
+// Get Total Expense by Date Range
+app.get('/expenses-by-date', (req: Request, res: Response) => {
+    try {
+        const database: { expenses: Array<IExpense> } = JSON.parse(fs.readFileSync('./database/db.json', 'utf-8'));
+
+        const range: any = req.query;
+        let totalExpense = 0;
+
+        database.expenses.forEach((item) => {
+            if (item.date >= range.startdate && item.date <= range.enddate) {
+                totalExpense += item.nominal;
+            }
+        })
+
+        res.status(200).send({
+            error: false,
+            message: 'Get Total Expense by Date Range Success!',
+            data: totalExpense
+        });
+
+    } catch (error: any) {
+        res.status(500).send({
+            error: true,
+            message: error.message
+        });
+    }
+})
+
+// Get Total Expense by Category
+app.get('/expenses-by-category', (req: Request, res: Response) => {
+    try {
+        const database: { expenses: Array<IExpense> } = JSON.parse(fs.readFileSync('./database/db.json', 'utf-8'));
+
+        const {category} = req.query;
+        let totalExpense = 0;
+
+        database.expenses.forEach((item) => {
+            if (item.category === category) {
+                totalExpense += item.nominal;
+            }
+        })
+
+        res.status(200).send({
+            error: false,
+            message: 'Get Total Expense by Category Success!',
+            data: totalExpense
+        });
+
+    } catch (error: any) {
+        res.status(500).send({
+            error: true,
+            message: error.message
+        });
+    }
+})
+
 
 app.listen(port, () => {
     console.log(`[SERVER] Server Running on Port ${port}`)
