2effff92b8596c4da013033484d2de5a178b3826
Aebroyx <41261433+Aebroyx@users.noreply.github.com>
2024-01-11 14:43:38 +0700

    gitignore root node_modules

--- Module 3 (Backend)/dashboard-api/dist/controllers/productController.js
+++ Module 3 (Backend)/dashboard-api/dist/controllers/productController.js
@@ -15,25 +15,40 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.createProduct = void 0;
 const connection_1 = __importDefault(require("../connection"));
 const createProduct = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
-    try {
-        const { name, price, description, stock } = JSON.parse(req.body.data);
-        yield connection_1.default.products.create({
-            data: {
-                name,
-                price,
-                description,
-                stock
+    yield connection_1.default.$transaction((tx) => __awaiter(void 0, void 0, void 0, function* () {
+        try {
+            const { name, price, description, stock } = JSON.parse(req.body.data);
+            const product = yield tx.products.create({
+                data: {
+                    name,
+                    price,
+                    description,
+                    stock
+                }
+            });
+            const createImages = [];
+            if (req.files) {
+                const filesArray = Array.isArray(req.files) ? req.files : req.files['bebas'];
+                filesArray.forEach((item) => {
+                    createImages.push({
+                        url: item.filename,
+                        products_id: product.id
+                    });
+                });
+                yield tx.productImages.createMany({
+                    data: createImages
+                });
             }
-        });
-        res.status(200).send({
-            error: false,
-            message: "Create Product Success",
-            data: null
-        });
-    }
-    catch (error) {
-        console.log(error);
-        next({ message: "Create Product Failed" });
-    }
+            res.status(200).send({
+                error: false,
+                message: "Create Product Success",
+                data: null
+            });
+        }
+        catch (error) {
+            console.log(error);
+            next({ message: "Create Product Failed" });
+        }
+    }));
 });
 exports.createProduct = createProduct;
--- Module 3 (Backend)/dashboard-api/src/controllers/productController.ts
+++ Module 3 (Backend)/dashboard-api/src/controllers/productController.ts
@@ -2,26 +2,45 @@ import { Request, Response, NextFunction } from "express";
 import prisma from "../connection";
 
 export const createProduct = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    try {
-        const {name, price, description, stock} = JSON.parse(req.body.data)
+    await prisma.$transaction(async (tx) => {
+        try {
+            const {name, price, description, stock} = JSON.parse(req.body.data)
+    
+            const product = await tx.products.create({
+                data: {
+                    name,
+                    price,
+                    description,
+                    stock
+                }
+            })
 
-        await prisma.products.create({
-            data: {
-                name,
-                price,
-                description,
-                stock
+            const createImages: any = []
+            
+            if (req.files) {
+            const filesArray = Array.isArray(req.files) ? req.files : req.files['bebas'];
+
+            filesArray.forEach((item: any) => {
+                createImages.push({
+                    url: item.filename,
+                    products_id: product.id
+                })
+            })
+
+            await tx.productImages.createMany({
+                data: createImages
+            })
             }
-        })
-
-        res.status(200).send({
-            error: false,
-            message: "Create Product Success",
-            data: null
-        })
-    }
-    catch (error){
-        console.log(error)
-        next({message: "Create Product Failed"})
-    }
+    
+            res.status(200).send({
+                error: false,
+                message: "Create Product Success",
+                data: null
+            })
+        }
+        catch (error){
+            console.log(error)
+            next({message: "Create Product Failed"})
+        }
+    })
 }
